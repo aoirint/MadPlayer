6e7ddc81a761160ccca4db4c07fdef3ab96a822e
aoirint <aoirint@gmail.com>
2021-03-07 04:40:53 +0900

    added onsen ui

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,42 @@
 import React from 'react'
-import logo from './logo.svg'
 import './App.css'
+
+import 'onsenui/css/onsenui.css'
+import 'onsenui/css/dark-onsen-css-components.css'
+import { Page, Toolbar, Button, BackButton, ToolbarButton, Icon } from 'react-onsenui'
+
+function help (): void {
+  console.log('hey!')
+}
+
+function renderToolbar (): JSX.Element {
+  return (
+    <Toolbar>
+      <div className='left'>
+        <BackButton>
+          Back
+        </BackButton>
+      </div>
+      <div className='center'>
+        Title
+      </div>
+      <div className='right'>
+        <ToolbarButton>
+          <Icon icon='md-menu' />
+        </ToolbarButton>
+      </div>
+    </Toolbar>
+  )
+}
 
 function App (): JSX.Element {
   return (
     <div className='App'>
-      <header className='App-header'>
-        <img src={logo} className='App-logo' alt='logo' />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className='App-link'
-          href='https://reactjs.org'
-          target='_blank'
-          rel='noopener noreferrer'
-        >
-          Learn React
-        </a>
-      </header>
+      <Page renderToolbar={renderToolbar}>
+        <Button onClick={help}>
+          Hello!
+        </Button>
+      </Page>
     </div>
   )
 }
